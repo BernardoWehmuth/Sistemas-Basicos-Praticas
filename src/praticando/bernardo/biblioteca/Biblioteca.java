26de73b02136832d785b016dd5dd8ef2d664f465
Bernardo Wehmuth <bernardowehmuth1109@gmail.com>
2025-08-19 15:45:45 -0300

    Atualizando 1

--- src/praticando/bernardo/biblioteca/Biblioteca.java
+++ src/praticando/bernardo/biblioteca/Biblioteca.java
@@ -3,9 +3,9 @@ package praticando.bernardo.biblioteca;
 public class Biblioteca {
 	public static void main(String[] args) {
 		Professor bernardo_wehmuth = new Professor("Bernardo Wehmuth", "107.128.389-88");
-		Livro batman = new Livro("BATMAN", "SILVA & SILVA");
-		Livro cesar = new Livro("CESAR", "CLARA & ROMARIO");
-		Livro heitor = new Livro("HEITOR", "ALVARO & CASTRO");
+		Livro batman = new Livro("Dom Quixote", "Miguel de Cervantes");
+		Livro cesar = new Livro("O Código Da Vinci", "Dan Brown");
+		Livro heitor = new Livro("Macbeth", "William Shakespeare");
 		bernardo_wehmuth.emprestarLivro(heitor);
 		bernardo_wehmuth.emprestarLivro(cesar);
 		bernardo_wehmuth.emprestarLivro(batman);
